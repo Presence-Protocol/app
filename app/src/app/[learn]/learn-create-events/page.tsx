dcdf4d8f1def77414e61aee57a9e2ea569c8c5eb
sven-hash <>
2025-01-28 18:53:43 +0100

    fix: github actions

--- app/src/app/[learn]/learn-create-events/page.tsx
+++ app/src/app/[learn]/learn-create-events/page.tsx
@@ -1,8 +1,16 @@
-'use client'
 
 import LandingLayout from "@/layouts/LandingLayout";
 import Navigation from "@/components/global/Navigation";
 import MintNFT from "@/components/nfts/MintNFT";
+
+// Add static paths generation
+export async function generateStaticParams() {
+  return [
+    {
+      learn: 'learn-create-events',
+    },
+  ]
+}
 
 export default function Mint() {
   return (
