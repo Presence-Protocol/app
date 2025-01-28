b38c600dd9e4eb93a6c7ad362c0c91d418a8f29f
sven-hash <>
2025-01-28 19:44:41 +0100

    fix: github actions

--- app/next.config.js
+++ app/next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   trailingSlash: true,
   reactStrictMode: true,
-  assetPrefix: process.env.NODE_ENV == "development" && "",
-  basePath: process.env.NODE_ENV == "development" && "",
+  assetPrefix: process.env.NODE_ENV != "development" ? "/app/" : "",
+  basePath: process.env.NODE_ENV != "development" ? "/app" : "",
   output: "export",
   distDir: 'build',
   experimental: {
