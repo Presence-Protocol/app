ea1baa03eb3d818c13121536153c542e2fe8defe
sven-hash <>
2025-01-28 17:27:23 +0100

    fix import

--- app/src/app/[learn]/learn-saving-memories/page.tsx
+++ app/src/app/[learn]/learn-saving-memories/page.tsx
@@ -2,14 +2,13 @@
 
 import LandingLayout from "@/layouts/LandingLayout";
 import Navigation from "@/components/global/Navigation";
-import NewMint from "@/components/forms/NewMint";
 import NFTList from "@/components/nfts/NFTList";
 
 export default function Mint() {
   return (
     <LandingLayout>
       <Navigation />
-      <NFTList />
+      <NFTList account={""} />
     </LandingLayout>
   )
 }
